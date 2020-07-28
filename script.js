187c033d49676dd3ad9aa7942603104a34588eb6
IvSan <foritun@gmail.com>
2020-07-28 14:26:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ const app = document.getElementById('root');
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
+
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://binaryjazz.us/wp-json/genrenator/v1/genre/', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(genre => {
+      const p = document.createElement('p');
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `You should like '${genre}'`;
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+}
+
+request.send();
